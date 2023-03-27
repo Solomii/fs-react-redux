3ff7354ed712529bca956f155665425812d24097
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-03-27 17:11:15 +0300

    Add countRedux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCount, subCount } from './store/countSlice';
 // import CounterReducer from "./components/CounterReducer"
 
 const App = () => {
-    return (
-        <>
-            <h1>Redux</h1>
-            {/* <CounterReducer/> */}
-            <div>
-                <h2>count:{0}</h2>
-                <button onClick={() => {}}>+</button>
-                <button onClick={() => {}}>-</button>
-            </div>
-        </>
-    );
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const handleAdd = () => dispatch(addCount());
+  const handleSub = () => dispatch(subCount());
+//   const handleStep = ({ targer: { value } }) => {
+//     Number(value);
+//   };
+  return (
+    <>
+      <h1>Redux</h1>
+      {/* <CounterReducer/> */}
+      <h2>count:{count}</h2>
+      <button onClick={handleAdd}>+</button>
+      <button onClick={handleSub}>-</button>
+      {/* <input type="number" value={step} onChange={handleStep} /> */}
+    </>
+  );
 };
 
 export default App;
