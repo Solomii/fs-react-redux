23cd5f9aa7bb74d3c82fb9298b1df09ad5b0bd95
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-03-27 14:36:54 +0300

    Add CounterReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import CounterReducer from "./components/CounterReducer"
 
 function App() {
     return (
-        <div className="App">
+        <>
             <h1>test</h1>
-        </div>
+            <CounterReducer/>
+        </>
     );
 }
 
